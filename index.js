51e13f4110e80176ffb7223a34d294b1e8fb5e66
Emmanuel Simon <musavili2022@gmail.com>
2024-09-16 21:08:37 +0300

    Version1

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Homelander');
+console.log('Another hero');
+console.log({
+    name:'Emmanuel Simon',
+    age:21,
+    gender: male
+})
+console.log('Version15');
+console.log('Version15');
